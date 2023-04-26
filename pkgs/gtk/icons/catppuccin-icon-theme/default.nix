4fd449b6913401963d350a5393cf72501102b6d7
Wittano <radoslaw.ratyna@gmail.com>
2023-04-26 18:28:34 +0200

    feat(gtk): add catppuccin icon theme

---
+++ pkgs/gtk/icons/catppuccin-icon-theme/default.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, stdenv, fetchFromGitHub }: stdenv.mkDerivation {
+  name = "catppuccin-gtk-icon-theme";
+
+  src = fetchFromGitHub {
+    owner = "Fausto-Korpsvart";
+    repo = "Catppuccin-GTK-Theme";
+    rev = "b8cffe7583876e17cc4558f32d17a072fa04ea9f";
+    sha256 = "sha256-wJnbXXWKX0mcqRYyE1Vs4CrgWXTwfk3kRC2IhKqQ0RI=";
+  };
+
+  propagatedBuildInputs = with pkgs; [ gtk-engine-murrine ];
+
+  installPhase = ''
+    mkdir -p $out/share/icons/catppuccin
+    cp -r ./icons/* $out/share/icons/catppuccin
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/ljmill/catppuccin-icons";
+    description = "Cattpucin GTK icon theme create by Fausto-Korpsvart";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ Wittano ];
+    platforms = platforms.linux;
+  };
+}
